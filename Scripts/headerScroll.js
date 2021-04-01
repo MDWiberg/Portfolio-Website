1fecc0595e8f186a1bc91563826f31bfa438e7bf
Matthew Wiberg <darianwiberg@yahoo.com>
2021-04-01 17:15:45 -0400

    Delete old commented out code

--- Scripts/headerScroll.js
+++ Scripts/headerScroll.js
@@ -7,12 +7,10 @@ let header = document.getElementById('intro-header');
 window.onscroll = () => {
   "use strict";
   if(document.body.scrollTop >= 25 || document.documentElement.scrollTop >= 25){
-    // header.style.backgroundColor = "black";
     header.classList.remove("transparent-background");
     header.classList.add("black-background");
   }
   else{
-    // header.style.backgroundColor = "transparent";
     header.classList.remove("black-background");
     header.classList.add("transparent-background");
   }
